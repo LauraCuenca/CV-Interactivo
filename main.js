b022e15c779e34be078ed98a215df92e60871df8
Cuenca Laura Antonina <laura.cuenca1@gmail.com>
2024-08-05 15:46:28 -0300

    agrego varios cambios

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navbarContainer = document.getElementById("navbar-container");
-    fetch("navbar.html")
-      .then(response => response.text())
-      .then(data => {
-        navbarContainer.innerHTML = data;
-  
-        // Agregar el evento al botón después de cargar la navbar
-        const darkModeToggle = document.getElementById("dark-mode-toggle");
-        if (darkModeToggle) {
-          darkModeToggle.addEventListener("click", function() {
-            document.body.classList.toggle("dark-mode");
-            // Cambia el icono según el modo
-            if (document.body.classList.contains("dark-mode")) {
-              darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>';
-            } else {
-              darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
-            }
-          });
-        }
-      })
-      .catch(error => console.error('Error al cargar la navbar:', error));
-  });
+  const navbarContainer = document.getElementById("navbar-container");
+
+  fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+      navbarContainer.innerHTML = data;
+
+      const darkModeToggle = document.getElementById("dark-mode-toggle");
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme === 'dark') {
+          document.body.classList.add("dark-mode");
+          if (darkModeToggle) {
+            darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+          }
+      } else {
+          if (darkModeToggle) {
+            darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+          }
+      }
+
+      if (darkModeToggle) {
+        darkModeToggle.addEventListener("click", function() {
+          document.body.classList.toggle("dark-mode");
+
+          if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem('theme', 'dark');
+            darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+          } else {
+            localStorage.setItem('theme', 'light');
+            darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+          }
+        });
+      }
+    })
+    .catch(error => console.error('Error al cargar la navbar:', error));
+});
